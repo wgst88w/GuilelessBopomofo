f60ac36dcdc8c65158cdc9ff2a265a1294fd74aa
wgst88w <wgst88w@Gmail.com>
2025-08-27 23:40:04 +0800

    Update build.gradle.kts
    Fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-
-    dependencies {
-        classpath(libs.gradle)
-        classpath(libs.kotlin.gradle.plugin)
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
+plugins {
+    id("com.android.application") version "8.6.0" apply false
+    id("com.android.library") version "8.6.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.2.0" apply false
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
 }
+
+
 
 tasks.register<Delete>("clean") {
     delete(layout.buildDirectory.get())
